b92a380086253e963065dce8a07ea299062f40f1
Vskelite <khamkarv01@gmail.com>
2024-08-08 22:38:47 +0530

    useState hook practice

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  let [counter, setCounter] = useState(7)
+  
+  const addValue = () => {
+    if( counter < 20){
+      setCounter(counter + 1);
+    }
+  }
+
+  const removeValue = () => {
+    if( counter > 0){
+      setCounter(counter - 1);
+    }
+  }
+
+  return (
+    <>
+      <h1>React counter using useState Hook</h1>
+      <h2>Counter value: {counter}</h2>
+      <button onClick={addValue}>Add value: {counter}</button>
+      <button onClick={removeValue}>Remove value: {counter}</button>
+    </>
+  )
+}
+
+export default App
